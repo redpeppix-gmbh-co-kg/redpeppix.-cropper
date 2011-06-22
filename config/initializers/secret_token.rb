3e2af29f1522a26033aa7ff70227764f165483e0
Christian Worreschk <christianworreschk@Christian-Worreschks-MacBook-Pro.local>
2011-06-22 15:53:19 +0200

    rails first version

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RedpeppixCropper::Application.config.secret_token = '171d256e42daeb6f21310bc8980775a7286731d66a06cc4e3a0b084624407dad9ed0c5d0fc9ea9f23871416e37d68f35a444b2ef7ef9bcd34969a895a378cd3c'
