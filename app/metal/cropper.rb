aa7331c64b0f5e4db91ee7c385580b8ece76bd22
Thies Schuster <schuster@redrauscher.de>
2012-01-06 10:24:17 +0100

    Cropper auf 2-Coords-Pix umgestellt

--- app/metal/cropper.rb
+++ app/metal/cropper.rb
@@ -11,50 +11,73 @@ class Cropper
         img_path = "#{Rails.root}/tmp/crops/images/img_#{pix.image.id}#{File.extname(pix.image.image_path)}"
         tmp_path = "#{Rails.root}/tmp/crops/#{pix.id}.tmp.jpg"
         final_path = "#{Rails.root}/tmp/crops/#{pix.id}.jpg"
+        debug = false
 
         coords = pix.coords.split(",")
-        padding = 10
+        padding = 30
         x_coords = []
         y_coords = []
-        coords.each_index do |x|
-          x.even? ? x_coords << coords[x].to_i : y_coords << coords[x].to_i
+        if coords.count == 2
+          x_coords << coords.first.to_i
+          y_coords << coords.last.to_i
+        else
+          coords.each_index do |x|
+            x.even? ? x_coords << coords[x].to_i : y_coords << coords[x].to_i
+          end
         end
 
-        left   = (x_coords.sort.first-padding) < 0 ? 0 : x_coords.sort.first-padding
-        top    = (y_coords.sort.first-padding) < 0 ? 0 : y_coords.sort.first-padding
-        width  = x_coords.sort.last + padding - left
-        height = y_coords.sort.last + padding - top
+        left   = [(x_coords.sort.first-padding), 0].max
+        top    = [(y_coords.sort.first-padding), 0].max
+        width  = coords.count == 2 ? padding*2 : (x_coords.sort.last + padding - left)
+        height = coords.count == 2 ? padding*2 : (y_coords.sort.last + padding - top)
+        if debug
+          Rails.logger.info("width: #{width}")
+          Rails.logger.info("height: #{height}")
+          Rails.logger.info("top: #{top}")
+          Rails.logger.info("left: #{left}")
+          Rails.logger.info(coords.to_yaml)
+          Rails.logger.info("Y-coords:")
+          Rails.logger.info(y_coords.sort.to_yaml)
+          Rails.logger.info("X-coords:")
+          Rails.logger.info(x_coords.sort.to_yaml)
+          Rails.logger.info("")
+          Rails.logger.info("(width-height)/2: #{(width-height)/2}") if width > height
+          Rails.logger.info("(height-width)/2: #{(height-width)/2}") if height > width
+          Rails.logger.info("top-^: #{top-(width-height)/2}") if width > height
+          Rails.logger.info("left-^: #{left-(height-width)/2}") if height > width
+          Rails.logger.info("curl -L -s -G #{pix.image.image_path} -o #{img_path}") unless File.exists?(img_path)
+        end
 
         # load image and save it locally if it does not exist
-        Rails.logger.info("curl -L -s -G #{pix.image.image_path} -o #{img_path}") unless File.exists?(img_path)
         system("curl -L -s -G #{pix.image.image_path} -o #{img_path}") unless File.exists?(img_path)
 
         # crop image using ImageMagick
 
         if width == height
-          
           # image is quadratic -> win
           command = "convert #{img_path} -crop #{width}x#{height}+#{left}+#{top}! #{tmp_path}"
         elsif width > height
-          
           # image is wider than high, top crop marker must be lowered by half the sides difference
-          command = "convert #{img_path} -crop #{width}x#{width}+#{left}+#{top-((height-width)/2)}! #{tmp_path}"
+          command = "convert #{img_path} -crop #{width}x#{width}+#{left}+#{top-((width-height)/2)}! #{tmp_path}"
         else
-          
           # image is higher than wide, left crop marker must be lowered by half the sides difference
           command = "convert #{img_path} -crop #{height}x#{height}+#{left-((height-width)/2)}+#{top}! #{tmp_path}"
         end
 
-        Rails.logger.info command
+        Rails.logger.info command if debug
         system(command)
 
         # put PIX-dot in center of the cropped image
         command = "composite -gravity center #{Rails.root}/dot.png #{tmp_path} #{final_path}"
-        Rails.logger.info command
+        Rails.logger.info command if debug
         system(command)
 
         # answer request with final image
-        [200, {"Content-Type" => "image/jpg"}, IO.read(final_path)]
+        if File.exists?(final_path)
+          [200, {"Content-Type" => "image/jpg"}, IO.read(final_path)]
+        else
+          [404, {}, []]
+        end
       else
         [404, {}, []]
       end
